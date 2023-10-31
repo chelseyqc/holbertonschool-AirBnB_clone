e7f0039795232413d509bf2b70bd0d8d7d4dce03
chelseyqc <6721@holbertonstudents.com>
2023-10-31 15:42:58 +0800

    feat: added file_storage and __init__ files and updated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 import json
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -22,6 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """prints the string specified"""
@@ -31,6 +33,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at to current"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,41 @@
+#!/usr/python3
+"""File Storage"""
+import json
+import sys
+import os
+
+
+class FileStorage:
+    """File Storage class"""
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        value = obj.__class__.__name__ + "." + str(obj.id)
+        self.__objects[value] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        empty_dict = {}
+        for key in self.__objects:
+            empty_dict[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w', encoding="utf-8") as file:
+            json.dump(empty_dict, file)
+
+    def reload(self):
+        """deserializes the JSON file to __objects if the JSON file exists"""
+        from models.base_model import BaseModel
+
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r', encoding="utf-8") as file:
+                reloaded = json.load(file)
+        else:
+            pass
+        for key, obj in reloaded.items():
+            self.__objects[key] = BaseModel(reloaded)
