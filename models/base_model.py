ec84bf8cc89f7ce985a563249ee181ee5a77852e
aliciastudies <6722@holbertonstudents.com>
2023-10-31 17:06:28 +1100

    feat: added args and kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,17 @@ from datetime import datetime
 class BaseModel:
     """BaseModel class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """instantiation of class attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) < 0:
+            for key, value in kwargs.items():
+                key == created_at or key == updated_at
+                value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, value, kwargs)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """prints the string specified"""
