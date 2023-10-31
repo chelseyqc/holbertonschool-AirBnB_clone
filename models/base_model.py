b734a7d60ce25e8cf484623d69e42775b2e49aa8
chelseyqc <6721@holbertonstudents.com>
2023-10-31 08:13:38 +0800

    fix: changed instance creation format

--- models/base_model.py
+++ models/base_model.py
@@ -11,10 +11,8 @@ class BaseModel:
     def __init__(self):
         """instantiation of class attributes"""
         self.id = str(uuid.uuid4())
-        current_create = datetime.now()
-        self.created_at = current_create.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        current_update = datetime.now()
-        self.updated_at = current_update.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """prints the string specified"""
@@ -23,11 +21,12 @@ class BaseModel:
 
     def save(self):
         """updates the public instance attribute updated_at to current"""
-        current_time = datetime.now()
-        self.updated_at = current_time.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
         new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
         new_dict["__class__"] = type(self).__name__
         return new_dict
