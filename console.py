b940f5077b24a8321faabfe6b4122edac82b5d28
aliciastudies <6722@holbertonstudents.com>
2023-11-01 21:46:27 +1100

    feat: add create(), show(), destroy()

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ import cmd
 from models import storage
 from models.base_model import BaseModel
 
+
 class HBNBCommand(cmd.Cmd):
     """basic commands """
 
@@ -33,11 +34,48 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, arg):
         """Prints the string representation of an instance
         based on the class name and id"""
-        if len(arg) == 0:
-            print("** class name missing **")
-        elif arg != "BaseModel":
-            print("** class doesn't exist **")
 
+        arg_list = arg.split()
+        if len(arg_list) == 0:
+            print("** class name missing **")
+        elif arg_list[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif (len(arg_list) < 2):
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(arg_list[0], arg_list[1])
+            class_dict = storage.all()
+            if key not in class_dict:
+                print("** no instance found **")
+            else:
+                print(class_dict[key])
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        arg_list = arg.split()
+        if len(arg_list) == 0:
+            print("** class name missing **")
+        elif arg_list[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif (len(arg_list) < 2):
+            print("** instance id missing **")
+        else:
+            this_key = "{}.{}".format(arg_list[0], arg_list[1])
+            class_dict = storage.all()
+            if this_key in class_dict:
+                class_dict.remove(this_key)
+            else:
+                print("** no instance found **")
+"""
+    def do_all(self, arg):
+        # Prints all string representation of all instances based
+        # or not on the class name
+        arg_list = arg.split(" ")
+        class_dict = storage.all()
+        new_list = []
+        if len(arg) == 0:
+            for value
+            """
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
