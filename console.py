35db33f8bd4d6c481e0bed67e3d12cc21966beb6
aliciastudies <6722@holbertonstudents.com>
2023-11-01 19:25:17 +1100

    fix: swapped order of quit() and emptyline()

--- console.py
+++ console.py
@@ -1,22 +1,42 @@
 #!/usr/bin/python3
 """command interpreter entry point"""
 import cmd
-
+from models import storage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """basic commands """
 
     prompt = '(hbnb) '
 
-    def if_emptyline(self):
-        """If command line is empty"""
-        pass
-
     def do_quit(self, arg):
         """Exits the program"""
         return True
 
+    def emptyline(self):
+        """If command line is empty"""
+        pass
+
     do_EOF = do_quit
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel & saves to json"""
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            instance = BaseModel()
+            instance.save()
+            print(instance.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance
+        based on the class name and id"""
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg != "BaseModel":
+            print("** class doesn't exist **")
 
 
 if __name__ == '__main__':
