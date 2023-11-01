a36feb633d4db06458ac9ebae503c418640bb623
chelseyqc <6721@holbertonstudents.com>
2023-11-01 18:12:24 +0800

    test: added test for State

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,11 @@
+#!/user/bin/python3
+"""class State unittests"""
+import unittest
+from models.state import State
+
+class TestState(unittest.TestCase):
+    """State unittests"""
+    def test_state_name(self):
+        state = State()
+        self.assertTrue(hasattr(state, "name"))
+        self.assertEqual(state.name, "")
